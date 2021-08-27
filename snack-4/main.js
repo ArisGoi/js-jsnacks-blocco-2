6bfbec8db7bac3fdca62c23c0837db39947c7cc3
ArisGoi <aris.goi97@gmail.com>
2021-08-27 18:01:40 +0200

    upload snack-4

---
+++ snack-4/main.js
@@ -0,0 +1,14 @@
+
+var numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+var somma = 0;
+
+for (i = 0; i < numeri.length; i++){
+
+    if (numeri[i] % 2 !== 0){
+        somma += numeri[i]
+    };
+
+}
+
+console.log(somma);
