8065f750bdf2a7276721e5dcdeaf72aeed736665
ArisGoi <aris.goi97@gmail.com>
2021-08-27 11:47:06 +0200

    snack-1 + while

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,13 +1,32 @@
-var numeri = [];
+// var numeri = [];
 
-for ( i = 0; i < 5; i++){
+// for ( i = 0; i < 5; i++){
+//     numeri.push(parseInt(prompt('inserisci un numero')))
+// };
+
+// var somma = 0;
+
+// for ( i = 0; i < 5; i++){
+//     somma += numeri[i] 
+// };
+// console.log(numeri);
+// console.log(somma);
+
+
+var numeri = [];
+var i = 0;
+while ( i < 5 ){
     numeri.push(parseInt(prompt('inserisci un numero')))
+
+    i++
 };
 
 var somma = 0;
 
-for ( i = 0; i < 5; i++){
+var i = 0;
+while ( i < 5){
     somma += numeri[i] 
+    i++
 };
 console.log(numeri);
 console.log(somma);
