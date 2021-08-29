b15dcceb76494909bb014f6732d861205f81a813
ArisGoi <aris.goi97@gmail.com>
2021-08-29 14:01:19 +0200

    upload snack-5

---
+++ snack-5/main.js
@@ -0,0 +1,11 @@
+var array01 = ["el-0", "el-1", "el-2", "el-3", "el-4", "el-5"];
+var array02 = ["el-0", "el-1"];
+
+var i = 2;
+while (array01.length > array02.length){
+    array02.push("el-" + i);
+    i++
+}
+
+console.log(array01);
+console.log(array02);
